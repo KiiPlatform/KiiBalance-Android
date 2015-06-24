8cdb479d524be16856caf9253928814b656e2f99
hrk fkm <fkm@fkmsoft.jp>
2015-06-24 17:22:03 +0900

    Remove unused imports

--- app/src/main/java/com/kii/sample/balance/MainActivity.java
+++ app/src/main/java/com/kii/sample/balance/MainActivity.java
@@ -16,8 +16,6 @@
 package com.kii.sample.balance;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
