6776ec1624fb4c754983877bd9b0e0cce505ea17
kishimotokii <hideaki.kishimoto@kii.com>
2017-04-06 18:52:12 +0900

    Modified file header/Correct button class

--- app/src/main/java/com/kii/sample/balance/list/BalanceListFragment.java
+++ app/src/main/java/com/kii/sample/balance/list/BalanceListFragment.java
@@ -1,18 +1,21 @@
-/*
- * Copyright 2013 Kii
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+//
+// Copyright 2017 Kii Corporation
+// http://kii.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
 package com.kii.sample.balance.list;
 
 import android.app.Activity;
@@ -20,6 +23,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -68,7 +72,7 @@ public class BalanceListFragment extends ListFragment {
     private static final int REQUEST_EDIT = 1001;
 
     @BindView(R.id.toolbar) Toolbar mToolBar;
-    @BindView(R.id.button_add) Button mAddButton;
+    @BindView(R.id.button_add) FloatingActionButton mAddButton;
     @BindView(R.id.text_remains_label) TextView mTotalTextLabel;
     @BindView(R.id.text_remains_value) TextView mTotalTextValue;
 
